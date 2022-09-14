7fc952c28e0976ac91bcfe7e783ae54d09b8f73a
shebahomedecor <112569778+shebahomedecor@users.noreply.github.com>
2022-09-14 12:31:49 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+//modal variables
+const modal= document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-madal-overlay]');
+
+//modal function
+const modalCloseFunc = function() {modal.classList.add('closed')}
+
+//modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
